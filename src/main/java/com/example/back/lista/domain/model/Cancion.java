6e62b336cc51af2d352d05dfdcf8fb5087ee94e1
Eider Muñoz <eider.munoz@asmetsalud.com>
2023-11-24 02:30:47 -0500

    Modelo lista

---
+++ src/main/java/com/example/back/lista/domain/model/Cancion.java
@@ -0,0 +1,14 @@
+package com.example.back.lista.domain.model;
+
+import jakarta.persistence.Entity;
+
+public class Cancion {
+    private int idCancion;
+    private String titulo;
+    private String artista;
+    private String album;
+    private String genero;
+    private int anno;
+
+
+}
---
+++ src/main/java/com/example/back/lista/domain/model/Lista.java
@@ -0,0 +1,10 @@
+package com.example.back.lista.domain.model;
+
+import java.util.List;
+
+public class Lista {
+    private int idLista;
+    private String nombre;
+    private String descripcion;
+    private List<Cancion> canciones;
+}
