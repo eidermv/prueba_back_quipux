abb29ffb4eba5ca2386c5619fb844dada2677a02
Eider Muñoz <eider.munoz@asmetsalud.com>
2023-11-25 02:28:35 -0500

    Creacion de Entity

---
+++ src/main/java/com/example/back/auth/infrastructure/db/entity/UsuariosLoginEnt.java
@@ -0,0 +1,19 @@
+package com.example.back.auth.infrastructure.db.entity;
+
+import com.example.back.auth.domain.model.UsuariosLogin;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "USUARIOS_LOGIN")
+public class UsuariosLoginEnt extends UsuariosLogin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_usuario", nullable = false)
+    private int idUsuario;
+
+}
