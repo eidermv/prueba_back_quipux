c2b424f92457aa56c094290312f454e0c0012472
Eider Muñoz <eider.munoz@asmetsalud.com>
2023-11-25 00:11:03 -0500

    Ajuste en modelo

--- src/main/java/com/example/back/lista/domain/model/Cancion.java
+++ src/main/java/com/example/back/lista/domain/model/Cancion.java
@@ -1,6 +1,6 @@
 package com.example.back.lista.domain.model;
 
-import jakarta.persistence.Entity;
+import java.util.Set;
 
 public class Cancion {
     private int idCancion;
@@ -9,6 +9,63 @@ public class Cancion {
     private String album;
     private String genero;
     private int anno;
+    private Set<ListaCancion> listaCancions;
+
+    public int getIdCancion() {
+        return idCancion;
+    }
+
+    public void setIdCancion(int idCancion) {
+        this.idCancion = idCancion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public void setAlbum(String album) {
+        this.album = album;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getAnno() {
+        return anno;
+    }
+
+    public void setAnno(int anno) {
+        this.anno = anno;
+    }
+
+    public Set<ListaCancion> getListaCancions() {
+        return listaCancions;
+    }
+
+    public void setListaCancions(Set<ListaCancion> listaCancions) {
+        this.listaCancions = listaCancions;
+    }
 
 
 }
--- src/main/java/com/example/back/lista/domain/model/Lista.java
+++ src/main/java/com/example/back/lista/domain/model/Lista.java
@@ -1,10 +1,42 @@
 package com.example.back.lista.domain.model;
 
-import java.util.List;
+import java.util.Set;
 
 public class Lista {
     private int idLista;
     private String nombre;
     private String descripcion;
-    private List<Cancion> canciones;
+    private Set<ListaCancion> listaCancions;
+
+    public int getIdLista() {
+        return idLista;
+    }
+
+    public void setIdLista(int idLista) {
+        this.idLista = idLista;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Set<ListaCancion> getListaCancions() {
+        return listaCancions;
+    }
+
+    public void setListaCancions(Set<ListaCancion> listaCancions) {
+        this.listaCancions = listaCancions;
+    }
 }
